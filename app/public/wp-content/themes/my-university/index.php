3da6aae75e6ce971ac4ec664f94a9432364795d3
Reese <65929791+bettameta@users.noreply.github.com>
2023-05-07 17:15:23 -0700

    Playing with arrays on a site as practice. Moving on to blog post displays next.

--- app/public/wp-content/themes/my-university/index.php
+++ app/public/wp-content/themes/my-university/index.php
@@ -1,1 +1,11 @@
-My New theme
+<?php
+
+   $names = array('Reese', 'Freddy', 'Henry', 'Rita');
+   $count = 0;
+
+   while($count < count($names)){
+      echo "<li>Hi my name is $names[$count]!</li>";
+      $count++;
+   }
+?> 
+
